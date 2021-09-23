4e9821fe3ac47056cd7c6ea5b09f8b62ca05e1e9
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2021-09-22 21:50:52 -0300

    add json server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,29 @@
-import styles from '../styles/home.module.scss';
+import { useEffect, useState } from 'react';
+
+interface IPost {
+  id: string;
+  title: string;
+}
 
 export default function Home() {
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/posts').then(response => {
+      response.json().then(data => {
+        setPosts(data);
+      });
+    });
+  }, []);
+  /*Client Side Render*/
   return (
     <div>
-      <h1>
-        Olá <span>Dev!</span>
-      </h1>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
