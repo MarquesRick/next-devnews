84551967845788dd832c9386cc27d1ce474ac0dc
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2021-09-22 22:20:58 -0300

    getStaticProps

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,35 @@
-export default function Posts() {
+import { GetStaticProps } from 'next';
+
+interface IPost {
+  id: string;
+  title: string;
+}
+
+interface IPostsProps {
+  posts: IPost[];
+}
+
+export default function Posts({ posts }: IPostsProps) {
   return (
     <div>
-      <h1>Posts</h1>
+      <h1>Listagem de Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+/**Gera as páginas de forma estática no build da aplicacao, ou seja, ao buildar realiza as consultas e monta o html */
+export const getStaticProps: GetStaticProps<IPostsProps> = async () => {
+  const response = await fetch('http://localhost:3333/posts');
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
