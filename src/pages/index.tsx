89a2cb17c781a9981126958fa637f3f6294ceec0
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2021-09-27 20:33:23 -0300

    Pagina Home parte 1 e 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,26 @@
-import { GetServerSideProps } from 'next';
-import { useEffect, useState } from 'react';
 import SEO from '../components/SEO';
+import styles from '../styles/home.module.scss';
 
-interface IPost {
-  id: string;
-  title: string;
-}
-
-interface IHomeProps {
-  posts: IPost[];
-}
-
-export default function Home({ posts }: IHomeProps) {
-  /*Client Side Render*/
-  // const [posts, setPosts] = useState<IPost[]>([]);
-
-  // useEffect(() => {
-  //   fetch('http://localhost:3333/posts').then(response => {
-  //     response.json().then(data => {
-  //       setPosts(data);
-  //     });
-  //   });
-  // }, []);
-
+export default function Home() {
   return (
-    <div>
-      <SEO title="Home" excludeTitleSuffix />
-      <h1>Posts</h1>
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <SEO title="Dev News!" excludeTitleSuffix />
+
+      <main className={styles.content}>
+        <section className={styles.section}>
+          <span>Olá Dev!</span>
+          <h1>
+            Bem-vindo e bem-vinda <br />
+            ao <span>Dev</span>News!
+          </h1>
+          <p>
+            Um blog com conteúdos extremamente <br />
+            <span>relevantes para o seu aprendizado.</span>
+          </p>
+        </section>
+
+        <img src="/home.svg" alt="Home image" />
+      </main>
+    </>
   );
 }
-
-/**Server side - tela montada no node */
-export const getServerSideProps: GetServerSideProps<IHomeProps> = async () => {
-  const response = await fetch('http://localhost:3333/posts');
-  const posts = await response.json();
-
-  return {
-    props: {
-      posts,
-    },
-  };
-};
